b3d1ca327904d38911d25dff63dbf4af69fa22ab
Camilo Galindo R <cgalindorivera@gmail.com>
2024-12-19 22:00:41 -0500

    nats

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,10 @@ async function bootstrap() {
 	const app = await NestFactory.createMicroservice<MicroserviceOptions>(
 		AppModule,
 		{
-			transport: Transport.TCP,
+			transport: Transport.NATS,
 			options: {
-				port: envs.port
-			}
+				servers: envs.nats_servers,
+			},
 		},
 	);
 
@@ -23,6 +23,6 @@ async function bootstrap() {
 	);
 
 	await app.listen();
-	logger.log('Products microservice running on port ' + envs.port)
+	logger.log('Products microservice running on port ' + envs.port);
 }
 bootstrap();
